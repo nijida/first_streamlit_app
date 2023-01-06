640b11b819bc66d776887249de74fc4f1c10beed
nijida <njnida@gmail.com>
2023-01-06 20:24:55 +0700

    Update streamlit_app.py
    Fix line space

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,4 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬 Kale, Spinach & Rocket Smoothie') 
 streamlit.text('🐔 Hard-Boiled Free-range Egg') 
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')​
