589a77b5954c1daf6a773d4f579c2f62a7e69f7e
nijida <njnida@gmail.com>
2023-01-07 11:12:25 +0700

    Update streamlit_app.py
    Correct sequence for fruit selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,13 @@ streamlit.text('🐔 Hard-Boiled Free-range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇') 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-fruits_selected = my_fruit_list.set_index('Fruit')
-fruits_to_show = my_fruit_list.loc[fruits_selected)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 #streamlit.dataframe(my_fruit_list)
 
 # Pick up list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries']) 
+Fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries']) 
+fruits_to_show = my_fruit_list.loc[fruits_selected)
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
