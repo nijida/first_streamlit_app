0d761c907188b5463281aa49b8f41c60c456e372
nijida <njnida@gmail.com>
2023-01-08 13:24:58 +0700

    Update streamlit_app.py
    Add snf data

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized) 
 
 import snowflake.connector
-
+my_cnx = snowflake.connector.connect(**streamlit.secret["snowflake"])
+my_cur = my_cnx.cursor() 
+my_cur.execute("select current_user(), current_account(), current_region()") 
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:") 
+streamlit.text(my_data_row) 
